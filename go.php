679a4d2ec86a43d7b87e94c88606c1e9e6f1e99f
cTu4 <33731408+cTu4@users.noreply.github.com>
2019-01-16 19:17:55 +0300

    f
    f

--- go.php
+++ go.php
@@ -1,2 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+
+</body> <!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Авторизация</title>
+</head>
+<style type="text/css">
+    body{
+        background: aliceblue;
+        margin: 0;
+        font-family: Calibri;
+    }
+    .container{
+        width: 300px;
+        height: 400px;
+        margin: auto;
+        text-align: center;
+    }
+
+    .field{
+        width: 100%;
+        height: 30px;
+        font-size: 14pt;
+        border: 2px solid darkgrey;
+        border-radius: 5px;
+        background-color: white;
+        color: black;
+        padding: 0 5px;
+    }
+    .field-container{
+        margin: 20px 0;
+    }
+    .button{
+        background-color: darkgrey;
+        color: white;
+        border: none;
+        cursor: pointer;
+        height: 30px;
+        border-radius: 5px;
+        padding: 0px 5px;
+    }
+    a{
+        text-decoration: none;
+        color: darkgrey;
+        font-size: 12px;
+    }
+
+</style>
+<body>
 <?php
-echo 'dk;gj';
+echo 'sgsh';
+?>
+</body>
+</html>
